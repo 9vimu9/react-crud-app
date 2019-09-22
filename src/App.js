2da444cdddf9353e7b3fd031e49f14ea142bc221
vimukthi <vimukthi@efuturesworld.com>
2019-09-22 20:25:26 +0530

    click event

--- src/App.js
+++ src/App.js
@@ -2,40 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    var head = "ggggg"
-    const list = ["fff","rrrr"]
+class App extends React.Component {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{head}</h1>
-          <h1>
-              {
-                  list.map(
-                      item=>{
-                          return (
-                              <div>{item}</div>
-                          );
-                      }
-                  )
-              }
-          </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    helloWorld()
+    {
+        console.log("ffffffffffffffffff")
+        alert("ffffffff");
+    }
+
+
+    render() {
+        var head ="gggg"
+        const list = ["232","fcadfaef"];
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>{head}</h1>
+                    <ul>
+                        {
+                            list.map(
+                                item => {
+                                    return (
+                                        <li key={item}>
+                                            <div onClick={this.helloWorld}>{item}</div>
+                                        </li>
+                                    );
+                                }
+                            )
+                        }
+                    </ul>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
