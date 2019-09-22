1548a9c21bfa4b8736a4714626b4fd429ec471f0
vimukthi <vimukthi@efuturesworld.com>
2019-09-23 01:19:40 +0530

    set prop type for component

--- src/componenets/MyComponent.js
+++ src/componenets/MyComponent.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {Component} from 'react';
+import PropTypes from "prop-types";
+
+const myComponentPropTypes ={
+    title:PropTypes.string.isRequired,
+    onClicked:PropTypes.func,
+    subTitle:PropTypes.string.isRequired
+};
 
 class MyComponent extends Component {
 
 
     render() {
 
-        const {title,subtitle,onClick} = this.props
+        const {title,subTitle,onClick} = this.props;
         return (
             <div className="MyComponent">
                 <h1>Title: {title}</h1>
-                <h2>sub Title: {subtitle}</h2>
+                <h2>sub Title: {subTitle}</h2>
                 <div onClick={onClick}>clcik me babes</div>
             </div>
         );
     }
 }
-
+MyComponent.prototypes = myComponentPropTypes;
 export default MyComponent;
