9b28597d90208ff15c11740f45e0079c968edbca
vimukthi <vimukthi@efuturesworld.com>
2019-09-22 15:20:27 +0530

    use varibales

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    var head = "ggggg"
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{head}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
